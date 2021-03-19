3f743d552dea4eaae6a43b6c1541fa35a4972185
Mohamed Nedal Abomokh <45313255+Mohamab29@users.noreply.github.com>
2021-03-19 19:10:52 +0200

    changed the background image and added some cards

---
+++ main.js
@@ -0,0 +1,8 @@
+// function selectText() {
+//     const myTextArea = document.getElementById('form-text');
+//     myTextArea.selectionStart = 11;
+// }
+function showDate() {
+    const myTimeDate = document.getElementById("date-time-input");
+    console.log(myTimeDate.value);
+}
